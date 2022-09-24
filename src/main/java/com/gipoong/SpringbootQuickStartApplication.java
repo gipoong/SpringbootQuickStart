1a76fdc752dcd8b16aa414463eeeaf23a597a95a
Gipoong <gipoong@naver.com>
2022-09-24 19:27:56 +0900

    Chapter 01

--- src/main/java/com/gipoong/SpringbootQuickStartApplication.java
+++ src/main/java/com/gipoong/SpringbootQuickStartApplication.java
@@ -2,12 +2,26 @@ package com.gipoong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.gipoong", "com.ruby"})
 public class SpringbootQuickStartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootQuickStartApplication.class, args);
+        // SpringApplication.run(SpringbootQuickStartApplication.class, args);
+
+        // 일반 자바 애플리케이션으로 실행
+        SpringApplication application =
+                new SpringApplication(SpringbootQuickStartApplication.class);
+
+        // WebApplication Type Setting
+        //application.setWebApplicationType(WebApplicationType.SERVLET);
+
+        // 배너 OFF
+        //application.setBannerMode(Banner.Mode.OFF);
+
+        application.run(args);
     }
 
 }
---
+++ src/main/java/com/ruby/controller/OtherController.java
@@ -0,0 +1,11 @@
+package com.ruby.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OtherController {
+
+    public OtherController() {
+        System.out.println("===> OtherController 생성");
+    }
+}
